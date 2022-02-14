f686c88a8737d02faa4434c382ad1e295dc9513b
BusterSalomon <54437784+BusterSalomon@users.noreply.github.com>
2022-02-14 09:20:30 +0100

    connected_sensors

---
+++ connected_range_sensors.ino
@@ -0,0 +1,70 @@
+
+// ------ CONSTANTS
+int ledPin = 17;
+int inputPin = A0;
+float coe = 1.344;
+
+// time
+unsigned long currentMillis;
+unsigned long prevMillis = 0;
+
+// ledState
+int ledState = LOW;
+
+// Get CM
+float getCM () {
+  int val = analogRead (inputPin);
+  float cm = val*coe;
+  return cm;
+}
+
+// Get interval
+int getInterval (float cm) {
+  int interval;
+  if (cm > 60) {
+    interval = 500;
+  }
+  if (cm < 60 && cm > 45) {
+    interval = 350;
+  }
+  if (cm < 45 && cm > 30) {
+    interval = 200;
+  }
+  if (cm < 30 && cm > 20) {
+    interval = 50;
+  }
+  if (cm < 20) {
+    interval = 0;
+  }
+  return interval;
+}
+
+// Blink
+void blinkWithMillis (int interval) {
+  // If interval == 0, led should always be on
+    currentMillis = millis();
+    // Blink if blink interval is reached
+    if (currentMillis - prevMillis >= interval) {
+      ledState = !ledState;
+      digitalWrite (ledPin, ledState);
+      prevMillis = currentMillis;  
+    }  
+}
+
+
+// Setup
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+  
+}
+
+// Loop
+void loop() {
+
+  float cm = getCM ();
+  int interval = getInterval (cm);
+  blinkWithMillis (interval);
+
+  
+}
